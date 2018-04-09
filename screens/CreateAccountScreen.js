cef1e1fc2fa29b498f713ad7e0558250a446fbc0
thebyronc <example@example.com>
2018-04-09 16:32:25 -0700

    add login and create account screens

--- screens/CreateAccountScreen.js
+++ screens/CreateAccountScreen.js
@@ -3,12 +3,12 @@ import { Text, View } from 'react-native';
 import TestImport from '../testimport/TestImport';
 import Styles from '../styles/styles';
 
-export default class LoginScreen extends React.Component {
+export default class CreateAccountScreen extends React.Component {
 
   render() {
     return (
       <View style={Styles.container}>
-        <Text style={Styles.headerText}>LOGIN</Text>
+        <Text style={Styles.headerText}>CREATE ACCOUNT</Text>
       </View>
     );
   }
